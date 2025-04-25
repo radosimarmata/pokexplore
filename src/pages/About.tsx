75fd9943b4ec6c1c82d614d6ceaa8fede3d19fbc
radosimarmata <simarmatarado@gmail.com>
2025-04-25 10:24:20 +0700

    add home | add about | add logo

---
+++ src/pages/About.tsx
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <div className="text-center">
+      <h1 className="text-2xl font-bold">About Pokexplore</h1>
+      <p>This is an app to explore Pokémon.</p>
+    </div>
+  )
+}
+
+export default About
---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div className="text-center">
+      <h1 className="text-2xl font-bold">Pokexplore</h1>
+      <p>This is an app to explore Pokémon.</p>
+    </div>
+  )
+}
+
+export default Home
